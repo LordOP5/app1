4c6df9db537baac7feab5994090d2f34cd00ee16
LordOP5 <110074683+LordOP5@users.noreply.github.com>
2023-10-06 15:40:34 +0700

    form-validation
    form-validation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import FormText from './form-text'
 import FormCheck from './form-check'
+import FormValidation from './form-validation'
 
 export default function App() {
-  return <FormCheck/>
+  return <FormValidation/>
 }
